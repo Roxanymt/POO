e57c87f97a8be0d274a1b25fd9ea0c55755e460d
Roxany Montenegro <roxanymontenegro@gmail.com>
2022-09-12 10:39:40 -0300

    clases

---
+++ Persona.py
@@ -0,0 +1,49 @@
+class Persona:
+    
+    pais = "Chile" #atributo de toda la clase
+    lista_personas= [] #lista de todas las instancias 
+    total_lineas_codigo = 0 #total de lineas de codigo de todos
+
+    def __init__(self, nombre, apellido, email, edad): #a través de init se inicializa la instancia. SELF incluye toda la información del objeto.
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.edad = edad
+        self.lineas_codigo = 0
+        Persona.lista_personas.append(self)
+
+    #métodos para instancais en especifico
+    def cumpleaños(self): #está a la misma altura de init
+        self.edad += 1
+        print("¡Muchas felicidades!")
+        if Persona.mayoria_edad(self.edad):
+            print("Wujuu, eres mayor de edad")
+
+
+    def codificar(self, abc):
+        #self.lineas_codigo += cantidad_lineas
+        self.lineas_codigo += abc
+        Persona.total_lineas_codigo += abc
+
+    #método genral que aplica a TODAS la instancia de la clase
+    @classmethod #indica que el sig método corresponde a toda la clase 
+    def imprime_lista(cls):  #cls = Persona (la clase)
+        print("Total de personas:", len (cls.lista_personas))
+        for persona in cls.lista_personas:
+            print(persona.nombre)
+
+    #método estático, función generica. no aplica especificamente a un objeto. 
+    @staticmethod
+    def mayoria_edad(edad):
+        if edad >= 18:
+            return True
+        else:
+            return False
+
+    def tomar_cerveza(self): #self = Bonnie
+        if Persona.mayoria_edad(self.edad):
+            print("Aquí esta tu cerveza",self.nombre)
+        else:
+            print("Lo siento",self.nombre,", no puedes beber")
+
+
---
+++ main.py
@@ -0,0 +1,34 @@
+from Persona import Persona  #from 'NOMBRE-ARCHIVO' import 'NOMBRE-CLASE'
+
+#Instancia = el objeto
+bonnie = Persona("Bonnie","Montenegro","bonnie@codingdojo.com",30)
+roxany = Persona("Roxany","Montenegro","roxany@codingdojo.com",26)
+
+print(bonnie.nombre)
+print(roxany.nombre)
+
+roxany.cumpleaños()
+
+print(bonnie.edad)
+print(roxany.edad)
+
+print(bonnie.lineas_codigo)
+print(roxany.lineas_codigo)
+
+bonnie.codificar(45)
+print(bonnie.lineas_codigo)
+
+print(bonnie.pais)
+print(roxany.pais)
+
+Persona.pais = "México" #se modifica atributo. "clase"."atributo" = "nuevo valor"
+print(roxany.pais)
+
+Persona.imprime_lista()
+
+pablo = Persona("Pablo","Picasso","pablo@codingdojo.com",50)
+
+Persona.imprime_lista()
+bonnie.cumpleaños()
+
+bonnie.tomar_cerveza()
